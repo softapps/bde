3a1bcf8766605d049f2df11856718b286033bfb4
root <root@jenkinssharedslave.compute.internal>
2017-12-18 21:51:03 +0530

    changed kdc settings

--- cmapi/kdc-cm.sh
+++ cmapi/kdc-cm.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 set -e
 echo "Post Installation Task For Kerberos - Cloudera Manager server"
-mv ~/cmf.keytab /etc/cloudera-scm-server/
-chown cloudera-scm:cloudera-scm /etc/cloudera-scm-server/cmf.keytab
-chmod 600 /etc/cloudera-scm-server/cmf.keytab
+
+mkdir /tmp/jce
+cd /tmp/jce
+wget wget http://10.0.10.251/repo/java/JCEPolicyJDK7/US_export_policy.jar
+wget http://10.0.10.251/repo/java/JCEPolicyJDK7/local_policy.jar
+mv /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/local_policy.jar /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/local_policy.jar.orig
+mv /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/US_export_policy.jar /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/US_export_policy.jar.orig
+cp /tmp/jce/local_policy.jar /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/local_policy.jar
+cp /tmp/jce/US_export_policy.jar /usr/java/jdk1.7.0_67-cloudera/jre/lib/security/US_export_policy.jar
+
+ 
+#mv cmf.keytab /etc/cloudera-scm-server/
+#chown cloudera-scm:cloudera-scm /etc/cloudera-scm-server/cmf.keytab
+#chmod 600 /etc/cloudera-scm-server/cmf.keytab
+
 echo "cloudera-scm/admin@COMPUTE.INTERNAL" > /etc/cloudera-scm-server/cmf.principal
 chown cloudera-scm:cloudera-scm /etc/cloudera-scm-server/cmf.principal
 chmod 600 /etc/cloudera-scm-server/cmf.principal
