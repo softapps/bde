c2f9136d2e286627cb68ae967931cc30a273cc24
root <root@jenkinssharedslave.compute.internal>
2017-12-19 17:21:19 +0530

    fixed KDC Server

--- cmapi/bootstrap-cm.sh
+++ cmapi/bootstrap-cm.sh
@@ -21,16 +21,16 @@ for DB in "${SCHEMA[@]}"; do
     done
 done
 
-echo "Set KDH keytab & principal"
-cd /tmp
-wget http://10.0.10.251/repo/kdc/cmf.tar.gz
-tar -xvf cmf.tar.gz
-mv /tmp/cmf.keytab /etc/cloudera-scm-server/
-chown cloudera-scm:cloudera-scm /etc/cloudera-scm-server/cmf.keytab
-chmod 600 /etc/cloudera-scm-server/cmf.keytab
+#echo "Set KDH keytab & principal"
+#cd /tmp
+#wget http://10.0.10.251/repo/kdc/cmf.tar.gz
+#tar -xvf cmf.tar.gz
+#mv /tmp/cmf.keytab /etc/cloudera-scm-server/
+#chown cloudera-scm:cloudera-scm /etc/cloudera-scm-server/cmf.keytab
+#chmod 600 /etc/cloudera-scm-server/cmf.keytab
 
-echo "cloudera-scm/admin@COMPUTE.INTERNAL" > /etc/cloudera-scm-server/cmf.principal
-chown cloudera-scm:cloudera-scm /etc/cloudera-scm-server/cmf.principal
-chmod 600 /etc/cloudera-scm-server/cmf.principal
+#echo "cloudera-scm/admin@COMPUTE.INTERNAL" > /etc/cloudera-scm-server/cmf.principal
+#chown cloudera-scm:cloudera-scm /etc/cloudera-scm-server/cmf.principal
+#chmod 600 /etc/cloudera-scm-server/cmf.principal
 
 while ! (exec 6<>/dev/tcp/$(hostname)/7180) 2> /dev/null ; do echo 'Waiting for Cloudera Manager to start accepting connections...'; sleep 10; done
--- cmapi/cmxDeploy.py
+++ cmapi/cmxDeploy.py
@@ -1017,12 +1017,12 @@ def enable_kerberos():
                       "SECURITY_REALM": cmx.kerberos['security_realm']})
 
     if cmx.api_version >= 11:
-        check.status_for_command("Configure Kerberos for Cluster",
+        check.status_for_command("Configure Kerberos for Cluster v11",
                                  cluster.configure_for_kerberos(datanode_transceiver_port=1004,
                                                                 datanode_web_port=1006))
-        check.status_for_command("Stop Cloudera Management Services", cm.get_service().stop())
-        # check.status_for_command("Wait for credentials to be generated", cm.generate_credentials())
-        check.status_for_command("Start Cloudera Management Services", cm.get_service().start())
+        check.status_for_command("Stop Cloudera Management Services v11", cm.get_service().stop())
+        check.status_for_command("Wait for credentials to be generated v11", cm.generate_credentials())
+        check.status_for_command("Start Cloudera Management Services v11", cm.get_service().start())
     else:
         hdfs = cdh.get_service_type('HDFS')
         zookeeper = cdh.get_service_type('ZOOKEEPER')
