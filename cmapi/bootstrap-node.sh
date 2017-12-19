d2a1a3239cd9b3295d8168a0270cd2e1a4666dcb
root <root@jenkinssharedslave.compute.internal>
2017-12-19 09:17:45 +0530

    fixed NTP Server

--- cmapi/bootstrap-node.sh
+++ cmapi/bootstrap-node.sh
@@ -8,8 +8,8 @@ wget http://10.0.10.251/repo/cdh593/cm5/redhat/$RELEASEVER/x86_64/cm/cloudera-ma
 yum install -y oracle-j2sdk* cloudera-manager-{daemons,agent}
 
 echo "Setup NTP"
-sed -i -e "s/server 0.centos.pool.ntp.org iburst/#/g" /etc/ntp.conf
-sed -i -e "s/server 1.centos.pool.ntp.org iburst/#/g" /etc/ntp.conf
-sed -i -e "s/server 2.centos.pool.ntp.org iburst/#/g" /etc/ntp.conf
-sed -i -e "s/server 3.centos.pool.ntp.org iburst/#/g" /etc/ntp.conf
+sed -i -e "s/server 0.centos.pool.ntp.org iburst/server 2.in.pool.ntp.org/g" /etc/ntp.conf
+sed -i -e "s/server 1.centos.pool.ntp.org iburst/server 0.asia.pool.ntp.org/g" /etc/ntp.conf
+sed -i -e "s/server 2.centos.pool.ntp.org iburst/server 1.asia.pool.ntp.org/g" /etc/ntp.conf
+sed -i -e "s/server 3.centos.pool.ntp.org iburst/server 2.asia.pool.ntp.org/g" /etc/ntp.conf
 service ntpd restart
